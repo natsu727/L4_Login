8c0bb4fd787a688c83b6e784087df5997ad272ac
Ubuntu <ubuntu@ip-172-31-71-109.ec2.internal>
2024-10-15 20:55:05 +0000

    fix: little fix

--- config/puma.rb
+++ config/puma.rb
@@ -34,6 +34,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# workers ENV.fetch("WEB_CONCURRENCY") {2}
+workers ENV.fetch("WEB_CONCURRENCY") {2}
 
-# preload_app!
+preload_app!
